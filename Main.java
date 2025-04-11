ef7ad4fc46af70b61fd88e2c5758f30d694eb31e
ThanakitRatt <55972thanakit@gmail.com>
2025-04-11 13:54:10 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class d0NtUseTh1s {
+
+public static void main(String[] aRgUmeNtz){ 
+int[] numbiez = {9, 3, 6, 1, 8, 2}; 
+badSort(numbiez);
+for(int x=0;x<numbiez.length;x++)
+System.out.print(numbiez[x]+" "); 
+}
+
+public static void badSort(int[] o_o){
+for(int i = 0; i < o_o.length; i++){
+for(int j = 1; j < o_o.length; j++){ 
+if(o_o[j] < o_o[j-1]){
+int tmp = o_o[j]; o_o[j]=o_o[i];
+o_o[i]= tmp;
+}
+}}}}
